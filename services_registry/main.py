d929048ae8fd8d63563fb6dfb91e0a9d8922d778
lushaojun <juno_250@live.com>
2020-09-04 02:06:07 +0800

    revert to ephemeral node

--- services_registry/main.py
+++ services_registry/main.py
@@ -1,4 +1,5 @@
 # -*- coding:utf-8 -*-
+import os
 from logger_helper import LoggerHelper
 from config_helper import ConfigHelper
 from register_service import RegisterCenter
@@ -9,15 +10,16 @@ if __name__ == "__main__":
     rs = RegisterCenter("{}:{}".format(host, port))
     rs.connect();
     try:
-        rs.service_register("hello_provider", "provider", {"host": "127.0.0.1", "port": 8000}, False)
-        rs.service_register("hello_provider", "provider", {"host": "127.0.0.1", "port": 8001}, False)
-        rs.service_register("hello_provider", "provider", {"host": "127.0.0.1", "port": 8002}, False)
+        rs.service_register("hello_provider", "provider", {"host": "127.0.0.1", "port": 8000})
+        rs.service_register("hello_provider", "provider", {"host": "127.0.0.1", "port": 8001})
+        rs.service_register("hello_provider", "provider", {"host": "127.0.0.1", "port": 8002})
         LoggerHelper().info(rs.get_register("hello_provider", "provider"))
         LoggerHelper().info("****")
         LoggerHelper().info(rs.get("/hello_provider/provider/ID0000000002"))
         rs.set("/hello_provider/provider/ID0000000002", b"123")
         LoggerHelper().info(rs.get("/hello_provider/provider/ID0000000002"))
         LoggerHelper().info(rs.get_children("/hello_provider/provider"))
+        os.system("pause")
     except Exception as e:
         LoggerHelper().error(e)
     finally:
